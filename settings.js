123cf2394d67b5289e4cebb6bc901bec7a6adc94
GayanthaLK <gayanthaprabod62@gmail.com>
2024-08-29 09:53:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,41 @@ const chalk = require('chalk')
 module.exports = {
   SESSION_ID: '',
   ALIVE_REACT: '👋',
-  ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
+  ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] *`𝙷𝙴𝚈, 𝙸'𝙼 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃.. 𝙼𝚈 𝙾𝚆𝙽𝙴𝚁 𝙽𝙰𝙼𝙴 𝙶𝙰𝚈𝙰𝙽𝚃𝙷𝙰 𝙿𝚁𝙰𝙱𝙾𝙳`*
+
+
+> *𝙸'𝙼 𝙰𝙻𝚆𝙰𝚈𝚂 𝙷𝙴𝚁𝙴 𝙵𝙾𝚁 𝙷𝙴𝙻𝙿 𝚈𝙾𝚄..❤️🤖 𝙻𝙾𝚅𝙴 𝚈𝙾𝚄 𝙵𝙾𝚁𝙴𝚅𝙴𝚁...*
+
+
+> *👇𝚃𝚈𝙿𝙴 𝚃𝙾 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂👇*
+
+- *`.𝙰𝙻𝙸𝚅𝙴`*  ⤑ `𝙲𝙷𝙴𝙲𝙺 𝚃𝙷𝙴 𝙱𝙾𝚃 𝙾𝙽`
+- *`.𝙼𝙴𝙽𝚄`*     ⤑ `𝙶𝙴𝚃 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙻𝙸𝚂𝚃`
+- *`.𝚂𝙾𝙽𝙶`*     ⤑ `𝚂𝙾𝙽𝙶 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳`
+- *`.𝚅𝙸𝙳𝙴𝙾`*   ⤑ `𝚅𝙸𝙳𝙴𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳`
+- *`.𝚈𝚃𝚂`*        ⤑ `𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝚂𝙴𝙰𝚁𝙲𝙷`
+- *`.𝙻𝙾𝙶𝙾`*     ⤑ `𝙻𝙾𝙶𝙾 𝙲𝚁𝙴𝙰𝚃𝙴`
+- *`.𝙵𝙱`*          ⤑ `𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳`
+- *`.𝚃𝙸𝙺𝚃𝙾𝙺`* ⤑ `𝚃𝙸𝙺𝚃𝙾𝙺 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳`
+- *`.𝙸𝙶`*          ⤑ `𝙸𝙽𝚂𝚃𝙴𝚁𝙶𝚁𝙰𝙼 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳`
+- *`.𝙶𝙳𝚁𝙸𝚅𝙴`* ⤑ `𝙶𝙾𝙾𝙶𝙻𝙴 𝙳𝚁𝙸𝚅𝙴 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳`
+- *`.𝙸𝙼𝙶`*        ⤑ `𝙸𝙼𝙰𝙶𝙴 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳`
+
+> *𝚈𝙾𝚄'𝚁𝙴 𝙲𝙰𝙿𝙰𝙱𝙻𝙴 𝙾𝙵 𝙳𝙾𝙸𝙽𝙶 𝙰 𝙻𝙾𝚃 𝙼𝙾𝚁𝙴 𝚆𝙾𝚁𝙺 𝙻𝙸𝙺𝙴 𝚃𝙷𝙸𝚂*
+
+> *👇𝙾𝚆𝙽𝙴𝚁 𝙽𝙰𝙼𝙴👇*
+*`𝙶𝙰𝚈𝙰𝙽𝚃𝙷𝙰 𝙿𝚁𝙰𝙱𝙾𝙳`*
+
+> *👇𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙾𝚆𝙽𝙴𝚁👇*
+*`https://wa.me/94773049310`*
+
+> *👇𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙶𝚁𝙾𝚄𝙿👇*
+*`https://chat.whatsapp.com/Hq6f54QY7ix1Jt2OfFTLWr`*
+
+> *👇𝙵𝙸𝙻𝙼 𝚆𝙾𝚁𝙻𝙳👇*
+*`https://chat.whatsapp.com/IfkRxWQBl1C61c1U4sEyTN`*
+
+> *`𝚃𝙷𝙰𝙽𝙺 𝚈𝙾𝚄 𝚄𝚂𝙴𝙸𝙽𝙶 𝙼𝚈 𝙱𝙾𝚃...🤖 𝙸'𝙼 𝚄𝚂𝙴𝙵𝚄𝙻𝙻 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃`*
 
 ⏰ Now time is @time
 📅 Date : @date
@@ -18,42 +52,42 @@ module.exports = {
 ☫ Work Type : @worktype
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
-  ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  ALIVE_MEDIA: 'https://telegra.ph/file/920e852caf373855c7564.jpg',
+  MONGODB_URL: 'mongodb+srv://gayanthaprabod72:ce24ome6OohZrPU6@gayan.oqqaeh3.mongodb.net/', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
-  BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
-  OWNER_NAME: 'ᴍ ʀ  ɴ ɪ ᴍ ᴀ',
-  OWNER_NUMBER: '94715166712',
-  SUDO: '94715166712',
+  BOT_NAME: '𝙼𝚂-𝚃𝙷𝙰𝚁𝚄',
+  OWNER_NAME: '𝙶𝙰𝚈𝙰𝙽𝚃𝙷𝙰 𝙿𝚁𝙰𝙱𝙾𝙳',
+  OWNER_NUMBER: '94773049310',
+  SUDO: '94773049310',
   WORK_TYPE: 'public',
-  WORK_MODE: 'group',
+  WORK_MODE: 'all',
   TIME_ZONE: 'Asia/Colombo',
-  CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴍᴅ',
-  STICKER_AUTHOR: 'Qᴜᴇᴇɴ ᴇʟɪꜱᴀ',
-  STICKER_PACK: 'ᴍʀ ɴɪᴍᴀ ᴏꜰᴄ',
+  CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ɢᴀʏᴀɴᴛʜᴀ ᴘʀᴀʙᴏᴅ',
+  STICKER_AUTHOR: '𝙼𝚂-𝚃𝙷𝙰𝚁𝚄',
+  STICKER_PACK: '𝙶𝙰𝚈𝙰𝙽𝚃𝙷𝙰 𝙾𝙵𝙲',
   REMOVE_BG_APIKEY: '',
   MAXIMUM_FILESIZE: 90,
   OWNER_R_EMOJI: '👽',
   ALIVE_AS_GIF: true,
   AUTO_VOICE: false,
   INBOX_BLOCK: false,
-  OWNER_REACT: true,
-  AUTO_REACT: true,
+  OWNER_REACT: false,
+  AUTO_REACT: false,
   ONLY_PM_GROUP_MSG_SEND: false,
   AUTO_SEEN_STATUS: true,
-  BOT_OFFLINE: false,
+  BOT_OFFLINE: true,
   READ_MESSAGE: false,
   ANTI_MSG_SPAM: false,
-  ANTI_BAD_WORD: false,
-  ANTI_VIEW_ONES: true,
-  ANTI_DELETE: true,
+  ANTI_BAD_WORD: true,
+  ANTI_VIEW_ONES: false,
+  ANTI_DELETE: false,
   BANED_USER_MSG_SEND: false,
   BLOCK_CMD_MSG_SEND: false,
   INBOX_BLOCK_MSG_SEND: false,
   CMD_CORRECT: true,
-  ANTI_CHANNEL_FORWARD: false,
+  ANTI_CHANNEL_FORWARD: true,
   ANTI_BOT: false,
   BUTTON_MESSAGES: true,
   SONG_DOWNLOAD_TYPE: 'selectable', // audio , document
